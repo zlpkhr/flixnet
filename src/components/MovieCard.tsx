80122123f620bcec0027e13261f7bf4386a0cb4e
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-02-12 01:36:01 +0100

    Remove side prop from HoverCardContent in MovieCard

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -42,7 +42,6 @@ export default function MovieCard({ movie }: MovieCardProps) {
         </div>
       </HoverCardTrigger>
       <HoverCardContent
-        side="right"
         className="w-80 border-white/10 bg-black/90 text-white"
       >
         <div className="space-y-2">
